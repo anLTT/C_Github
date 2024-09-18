ac51c12d38091f8c71137d0dd49a1e85ea1aacc6
anLTT <2396029174@qq.com>
2024-09-18 15:44:23 +0800

    C point homework

---
+++ VS/Homework_point/Homework_point/test.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include "string.h"
+
+//单身狗1
+int main()
+{
+    int arr[] = { 1,2,3,4,5,1,2,3,4 };
+    int sz = sizeof(arr) / sizeof(arr[0]);
+    int num = 0;
+    int count = 0;
+    for (int i = 0; i < sz; i++)
+    {
+        num = arr[i];
+        for (int j = i + 1; j < sz; j++)
+        {
+            if (num ^ arr[j] == 1)
+            {
+                count++;
+            }
+        }
+        if (count != sz - i - 1)
+        {
+            break;
+        }
+    }
+
+
+    printf("数字%d只出现一次", num);
+
+    return 0;
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
